ba1f45563e0a6864bb39e5f229d709977981422a
Rui Alves <up201606746@fe.up.pt>
2019-08-24 11:25:09 +0100

    Fix files according to new eslint rules

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,15 +5,15 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+    const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -21,32 +21,32 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
-  return (
+    return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
+          style={{
+              margin: "0 auto",
+              maxWidth: 960,
+              padding: "0px 1.0875rem 1.45rem",
+              paddingTop: 0,
+          }}
       >
-        <main>{children}</main>
-        <footer>
+          <main>{children}</main>
+          <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+              {" "}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
       </div>
     </>
-  )
-}
+    );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
