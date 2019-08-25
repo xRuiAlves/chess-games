9225731e607a9b972db1e486cf3f20420ae0ddff
Rui Alves <up201606746@fe.up.pt>
2019-08-25 10:31:43 +0100

    Refactor 404 not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -6,8 +7,11 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h2>Not found</h2>
+        <p>
+            The requested page does seem to exist. Please return to
+            the <Link className="emphasized-anchor" to="/">home page</Link>.
+        </p>
     </Layout>
 );
 
