0a31e60e3885a4eb94ab7898304f7c91e01d03ab
Rui Alves <up201606746@fe.up.pt>
2019-09-10 21:11:07 +0100

    Fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/footer.css";
 
-const Footer = () => (
-    <footer>
-      © {new Date().getFullYear()}, made by Rui Alves
-    </footer>
-);
+class Footer extends Component {
+    componentDidMount() {
+        const page_height = document.body.getBoundingClientRect().height;
+        const viewport_height = window.innerHeight || document.documentElement.clientHeight || screen.height;
+        document.getElementById("footer").style.top = `${Math.max(viewport_height, page_height) - 30}px`;
+    }
+
+    render() {
+        return (
+            <footer id="footer">
+                © {new Date().getFullYear()}, made by Rui Alves
+            </footer>
+        );
+    }
+}
 
 export default Footer;
