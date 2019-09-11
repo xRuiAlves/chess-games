ccee51b3d66b35fd3d0609d1b6351ef16416c4c2
Rui Alves <up201606746@fe.up.pt>
2019-09-11 13:40:20 +0100

    Sort games by date

--- src/components/gamesList.js
+++ src/components/gamesList.js
@@ -5,9 +5,18 @@ import GameItem from "./gameItem.js";
 const GamesList = () => (
         <>
             <h2>Games List</h2>
-            {games.map((game, i) => (
-                <GameItem key={i} {...game} />
-            ))}
+            {games
+                .sort((game1, game2) => {
+                    const date1 = parseInt(`${game1.date.substr(6, 4)}${game1.date.substr(3, 2)}${game1.date.substr(0, 2)}`, 10);
+                    const date2 = parseInt(`${game2.date.substr(6, 4)}${game2.date.substr(3, 2)}${game2.date.substr(0, 2)}`, 10);
+                    if (game1.round && game2.round && date1 === date2) {
+                        return parseInt(game2.round, 0) - parseInt(game1.round, 0);
+                    }
+                    return date2 - date1;
+                })
+                .map((game, i) => (
+                    <GameItem key={i} {...game} />
+                ))}
         </>
 );
 
