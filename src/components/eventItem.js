24d4f800dbd462e090a6ad544110719e82e24d52
Rui Alves <up201606746@fe.up.pt>
2019-10-05 15:14:44 +0100

    Add watermark to beautify event and game items

--- src/components/eventItem.js
+++ src/components/eventItem.js
@@ -2,30 +2,32 @@ import React from "react";
 import { Link } from "gatsby";
 import { buildEventUrl } from "../utils.js";
 import "../css/game.css";
+import Watermark from "../css/images/watermark.jpg";
 
 const EventItem = ({ name, start, end, rounds }) => (
     <div className="game-item event-item">
-        <Link to={buildEventUrl(name)}>
-            <p className="game-header">
-                <strong>{name}</strong>
-            </p>
-            {start === end ?
-                <p>
-                    <strong>Date: </strong>{start}
-                </p>
-                :
-                <div>
-                    <p>
-                        <strong>Start: </strong>{start}
-                    </p>
-                    <p>
-                        <strong>End: </strong>{end}
-                    </p>
-                </div>
-            }
+        <p className="game-header">
+            <strong>{name}</strong>
+        </p>
+        {start === end ?
             <p>
-                <strong>Rounds: </strong>{rounds}
+                <strong>Date: </strong>{start}
             </p>
+            :
+            <div>
+                <p>
+                    <strong>Start: </strong>{start}
+                </p>
+                <p>
+                    <strong>End: </strong>{end}
+                </p>
+            </div>
+        }
+        <p>
+            <strong>Rounds: </strong>{rounds}
+        </p>
+        <Link to={buildEventUrl(name)}>
+            <img src={Watermark} alt="" />
         </Link>
     </div>
 );
--- src/components/gameItem.js
+++ src/components/gameItem.js
@@ -2,43 +2,45 @@ import React from "react";
 import { Link } from "gatsby";
 import { buildGameUrl, prettifyPlayerData } from "../utils.js";
 import "../css/game.css";
+import Watermark from "../css/images/watermark.jpg";
 
 const GameItem = (game) => (
     <div className="game-item">
-        <Link to={buildGameUrl(game)}>
-            <div className="game-header">
-                <p>
-                    <span className="game-players">
-                        <strong>{game.white.title && `${game.white.title} `}{prettifyPlayerData(game.white)}</strong>
-                        {" "}vs{" "}
-                        <strong>{game.black.title && `${game.black.title} `}{prettifyPlayerData(game.black)}</strong>
-                    </span>
-                </p>
+        <div className="game-header">
+            <p>
+                <span className="game-players">
+                    <strong>{game.white.title && `${game.white.title} `}{prettifyPlayerData(game.white)}</strong>
+                    {" "}vs{" "}
+                    <strong>{game.black.title && `${game.black.title} `}{prettifyPlayerData(game.black)}</strong>
+                </span>
+            </p>
 
-                {game.table && game.white.club && game.black.club &&
+            {game.table && game.white.club && game.black.club &&
                     <p>
                         <strong>{game.white.club}</strong>
                         <span> vs </span>
                         <strong>{game.black.club}</strong>
                     </p>
-                }
-            </div>
+            }
+        </div>
 
-            <p>
-                <strong>Date: </strong>{game.date}
-            </p>
+        <p>
+            <strong>Date: </strong>{game.date}
+        </p>
 
-            {game.round &&
+        {game.round &&
                 <p>
                     <strong>Round: </strong>{game.round}
                 </p>
-            }
+        }
 
-            {game.table &&
+        {game.table &&
                 <p>
                     <strong>Table: </strong>{game.table}
                 </p>
-            }
+        }
+        <Link to={buildGameUrl(game)}>
+            <img src={Watermark} alt="" />
         </Link>
     </div>
 );
