7f106ee770f328e4d8f6f80a985c51f473d17025
Rui Alves <up201606746@fe.up.pt>
2019-12-24 15:36:49 +0000

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,11 @@ module.exports = {
                 icon: "src/images/logo.png", // This path is relative to the root of the site.
             },
         },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "UA-154963609-1",
+            },
+        },
     ],
 };
